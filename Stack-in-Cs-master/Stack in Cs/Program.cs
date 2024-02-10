967d0e3f274657641ac4b834b55b7483de9d25d1
89orpnoN <115448007+89orpnoN@users.noreply.github.com>
2024-02-10 10:13:21 +0100

    aggiunto stack e albero binario

---
+++ Stack-in-Cs-master/Stack in Cs/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var stack = new Steck<int>(34);
+
+stack.Push(43);
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+stack.Push(4);
+
+stack.Pop();
+stack.Push(5);  
